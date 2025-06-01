78b57e3357e1a3d7984809b3e2ee6e0a8ee6593e
Shiny003 <dextermorley27@gmail.com>
2025-06-01 13:37:29 -0600

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using GorillaNetworking;
+using UnityEngine;
+
+namespace CosmeticGUI
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public Rect cosmeticsRect = new Rect(400, 10, 120, 90);
+
+        private void OnGUI()
+        {
+            cosmeticsRect = GUI.Window(1, cosmeticsRect, CosmeticsWindow, $"Cosmetic GUI");
+        }
+
+        private string _cosmetic;
+        private void CosmeticsWindow(int windowID)
+        {
+            _cosmetic = GUI.TextArea(new Rect(10, 20, 100, 20), _cosmetic);
+            if (GUI.Button(new Rect(10, 40, 100, 20), "Purchase"))
+            {
+                foreach (var item in CosmeticsController.instance.allCosmetics)
+                {
+                    if (item.itemName == _cosmetic)
+                    {
+                        CosmeticsController.instance.itemToBuy = item;
+                        CosmeticsController.instance.PurchaseItem();
+                        UpdateCosmetics();
+                    }
+                }
+            }
+
+            if (GUI.Button(new Rect(10, 60, 100, 20), "Try On"))
+            {
+                var item = CosmeticsController.instance.allCosmeticsDict[_cosmetic];
+                
+                if (CosmeticsController.instance.currentCart.Contains(item))
+                {
+                    GorillaTagger.Instance.offlineVRRig.concatStringOfCosmeticsAllowed.Replace(item.itemName, "");
+                    CosmeticsController.instance.currentCart.Remove(item);
+                }
+                else
+                {
+                    GorillaTagger.Instance.offlineVRRig.concatStringOfCosmeticsAllowed += item.itemName;
+                    CosmeticsController.instance.currentCart.Add(item);
+                }
+                UpdateCosmetics();
+            }
+            
+            GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+        }
+
+        private void UpdateCosmetics()
+        {
+            var instance = CosmeticsController.instance;
+            instance.UpdateCurrencyBoard();
+            instance.UpdateMyCosmetics();
+            instance.UpdateShoppingCart();
+            instance.UpdateWardrobeModelsAndButtons();
+            instance.UpdateWornCosmetics();
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace CosmeticGUI
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "ShinyGorilla.CosmeticGUI";
+        public const string Name = "CosmeticGUI";
+        public const string Version = "1.0.0";
+    }
+}
